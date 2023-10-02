e7e929d93bca4994e426639e5d002b3ab9080b50
victor <124385408+77bueno@users.noreply.github.com>
2023-10-02 11:27:43 -0300

    .

--- index.php
+++ index.php
@@ -10,13 +10,44 @@
     <article>
         <h2>Quem somos</h2>
 
-        <p>Impulsionamos sua carreira de freelancer rumo ao sucesso, conectamos talentos e oportunidades! Nós somos a CODELANCE, a plataforma líder em FreeLance na área de tecnologia. Encontre sua carreira dos sonhos conosco!</p>
+        <p>Somos uma plataforma dedicada a reunir os talentos mais brilhantes da área de desenvolvimento web com os projetos mais desafiadores e empolgantes. Somos apaixonados por tecnologia e acreditamos no poder das mentes criativas para transformar ideias em realidade</p>
     </article>
         
     <section>
-        <p>Conectando talentos e oportunidades! Nós somos a CODELANCE, a plataforma líder em FreeLance na área de tecnologia. Encontre sua carreira dos sonhos conosco! </p>
+        <h3>O que fazemos</h3>
 
-        <p>Descubra os melhores talentos em programação na nossa plataforma. Conectamos sua empresa a freelancers programadores altamente qualificados, prontos para impulsionar seus projetos com eficiência e inovação. Experimente hoje!</p>
+        <details>
+            <summary> Nossa Missão: </summary>
+                <ul>
+                    <li>Capacitar Desenvolvedores Talentosos: Acreditamos que cada desenvolvedor possui um conjunto único de habilidades. O que fazemos é conectar esses talentos a oportunidades que os inspirem e desafiem, permitindo-lhes crescer e prosperar.</li>
+
+                    <li>Potencializar Projetos Inovadores: Colaboramos com empresas, startups e organizações visionárias que buscam transformar ideias em resultados tangíveis. Fornecemos acesso direto a uma comunidade diversificada de desenvolvedores de alto calibre para impulsionar projetos a novos patamares de sucesso.</li>
+                </ul> 
+        </details>
+        
+
+        <h3>Por Que a CodeLance?</h3>
+        <p>Estamos aqui para simplificar o processo de encontrar os talentos certos para seus projetos, economizando seu tempo e recursos. Deixe-nos cuidar dos detalhes para que você possa se concentrar no que faz de melhor: alcançar seus objetivos.</p>
+        </section>
+
+        <section>
+            <h2>Áreas de Atuação</h2>
+            <p>A Codelance se destaca como a única plataforma de freelancers para desenvolvedores. Oferecemos um ambiente inovador onde talentos técnicos podem encontrar projetos sob medida, enquanto empresas acessam expertise de alta qualidade. Nossa abordagem revolucionária impulsiona o sucesso de freelancers e clientes, moldando o futuro do trabalho independente. </p>
+
+            <h4>Desenvolvedores</h4>
+            <p>Desenvolvedor Front, Back, 
+            Mobile ou Full-Stack</p>
+
+            <h4>Linguagens de Programação mais usadas</h4>
+            <p>JavaScript, Python, Php, Java, C++, C#</p>
+
+            <h4>Ferramentas mais usadas</h4>
+            <p>Visual Studio Code, WordPress, PhpStorm </p>
+        </section>
+
+        <section>
+            <p>PRONTO PARA COMEÇAR?</p>
+            <button type="submit">faça login e comece sua jornada</button>
         </section>
     </main>
 <script src="js/menu.js"></script>
