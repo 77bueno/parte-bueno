61298ecfa457789afadbc26a521e97bb85238cbb
victor <124385408+77bueno@users.noreply.github.com>
2023-10-11 11:16:31 -0300

    Css da Index

--- index.php
+++ index.php
@@ -14,32 +14,22 @@
     <section class="o-que-fazemos">
         <h2>O que fazemos</h2> 
                 
-        <p>Capacitar Desenvolvedores Talentosos: Acreditamos que cada desenvolvedor possui um conjunto único de habilidades. O que fazemos é conectar esses talentos a oportunidades que os inspirem e desafiem, permitindo-lhes crescer e prosperar.</p>
-
-        <p>Estamos aqui para simplificar o processo de encontrar os talentos certos para seus projetos, economizando seu tempo e recursos. Deixe-nos cuidar dos detalhes para que você possa se concentrar no que faz de melhor: alcançar seus objetivos.</p>
-        
+        <p>Na Codelance, facilitamos a jornada dos freelancers de desenvolvimento. Nossa plataforma inteligente é projetada para simplificar a busca por projetos que correspondam às suas habilidades exclusivas e interesses pessoais. Oferecemos uma ampla gama de categorias de desenvolvimento, desde sites para web até soluções avançadas para aplicações mobiles. Encontrar projetos sob medida para você nunca foi tão fácil.<p>
     </section>
 
     <article class="quem-somos">
-        <h2 class="corp">Quem somos</h2>
         <img src="imagens/ilustracao-1.svg" alt="">
 
+        <h2 class="qs-titulo">Quem somos</h2>
         <section>
-            <p>Somos uma plataforma dedicada a conectar talentosos freelancers e empresas em busca de profissionais qualificados. Nosso objetivo é tornar o processo de encontrar trabalho ou contratar talento mais simples e eficiente.</p>
-
-            <p>Para freelancers, oferecemos uma plataforma onde você pode criar um perfil, mostrar suas habilidades e experiência, e se candidatar a projetos emocionantes em diversas áreas. Seja você um desenvolvedor web, designer gráfico, redator ou qualquer outro profissional, aqui você encontrará oportunidades que se alinham com suas paixões e habilidades.</p>
-
-            <p>Para empresas, oferecemos uma maneira fácil de postar vagas e encontrar os candidatos ideais. Nossa comunidade de freelancers é diversificada e altamente qualificada, pronta para contribuir para o sucesso de seu projeto.</p>
-
-            <p>Nosso compromisso é facilitar o encontro entre talento e oportunidades, tornando o processo de contratação ou a busca por projetos autônomos uma experiência fluida e produtiva.</p>
-
-            <p>Junte-se a nós e faça parte de uma comunidade de freelancers e empresas que estão prontos para colaborar e prosperar. Vamos ajudar você a encontrar os talentos certos ou as oportunidades ideais, simplificando a forma como o trabalho é feito.</p>
+            <p>A Codelance é uma comunidade dedicada de desenvolvedores independentes apaixonados por tecnologia e inovação. Nossa missão é proporcionar oportunidades de sucesso para freelancers, conectando-os a projetos desafiadores em um ambiente de colaboração e crescimento mútuo. Junte-se a nós e faça parte desta comunidade que valoriza suas habilidades e aspirações. Aqui, você não está sozinho em sua jornada de desenvolvimento profissional.</p>
         </section>
     </article>
         
     
 
-        <section>
+        <section class="areas-de-atuacao">
+
             <h2>Áreas de Atuação</h2>
             <p>A Codelance se destaca como a única plataforma de freelancers para desenvolvedores. Oferecemos um ambiente inovador onde talentos técnicos podem encontrar projetos sob medida, enquanto empresas acessam expertise de alta qualidade. Nossa abordagem revolucionária impulsiona o sucesso de freelancers e clientes, moldando o futuro do trabalho independente. </p>
 
@@ -54,7 +44,7 @@
             <p>Visual Studio Code, WordPress, PhpStorm </p>
         </section>
 
-        <section>
+        <section class="pt-comecar">
             <p>PRONTO PARA COMEÇAR?</p>
             <button type="submit">faça login e comece sua jornada</button>
         </section>
