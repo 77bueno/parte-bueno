e443e28a09334234d26d6e4f5c404bca8a368baa
victor <victorhugomotabueno@gmail.com>
2023-10-22 22:51:31 -0300

    Atualizações na index

--- index.php
+++ index.php
@@ -22,30 +22,41 @@
     </section>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#001b42" fill-opacity="1" d="M0,256L120,224C240,192,480,128,720,133.3C960,139,1200,213,1320,250.7L1440,288L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path></svg>
     <article class="quem-somos">
-        <img src="imagens/ilustracao1.svg" alt="">
+        <img data-aos="zoom-in" src="imagens/ilustracao1.svg" alt="ilustração do site">
 
-        <h2 class="qs-titulo">Quem somos</h2>
+        <h2 data-aos="fade-up" class="qs-titulo">Quem somos</h2>
         <section>
-            <p>A Codelance é uma comunidade dedicada de desenvolvedores independentes apaixonados por tecnologia e inovação. Nossa missão é proporcionar oportunidades de sucesso para freelancers, conectando-os a projetos desafiadores em um ambiente de colaboração e crescimento mútuo. Junte-se a nós e faça parte desta comunidade que valoriza suas habilidades e aspirações. Aqui, você não está sozinho em sua jornada de desenvolvimento profissional.</p>
+            <p data-aos="fade-up">A Codelance é uma comunidade dedicada de desenvolvedores independentes apaixonados por tecnologia e inovação. Nossa missão é proporcionar oportunidades de sucesso para freelancers, conectando-os a projetos desafiadores em um ambiente de colaboração e crescimento mútuo. Junte-se a nós e faça parte desta comunidade que valoriza suas habilidades e aspirações. Aqui, você não está sozinho em sua jornada de desenvolvimento profissional.</p>
         </section>
+    </article>
+
+    <section class="pt-comecar">
+            
+    </section>
+
+    <article class="pq-codelance">
+        <img data-aos="zoom-in" src="imagens/ilustracao2.svg" alt="ilustração do site">
+
+        <h2 data-aos="fade-up" class="pc-titulo">Por Que a codelance?</h2>
+        <p data-aos="fade-up">Estamos aqui para simplificar o processo de encontrar os talentos certos para seus projetos, economizando seu tempo e recursos. Deixe-nos cuidar dos detalhes para que você possa se concentrar no que faz de melhor: alcançar seus objetivos.</p>
     </article>
         
     
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#001b42" fill-opacity="1" d="M0,32L80,69.3C160,107,320,181,480,176C640,171,800,85,960,58.7C1120,32,1280,64,1360,80L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
         <section class="areas-de-atuacao">
             
-            <h2>Áreas de Atuação</h2>
-            <p>A Codelance se destaca como a única plataforma de freelancers para desenvolvedores. Oferecemos um ambiente inovador onde talentos técnicos podem encontrar projetos sob medida, enquanto empresas acessam expertise de alta qualidade. Nossa abordagem revolucionária impulsiona o sucesso de freelancers e clientes, moldando o futuro do trabalho independente. </p>
+            <h2 data-aos="fade-up">Áreas de Atuação</h2>
+            <p data-aos="fade-up">A Codelance se destaca como a única plataforma de freelancers para desenvolvedores. Oferecemos um ambiente inovador onde talentos técnicos podem encontrar projetos sob medida, enquanto empresas acessam expertise de alta qualidade. Nossa abordagem revolucionária impulsiona o sucesso de freelancers e clientes, moldando o futuro do trabalho independente. </p>
 
-            <h4>Desenvolvedores</h4>
-            <p>Desenvolvedor Front, Back, 
+            <h4 data-aos="fade-up">Desenvolvedores</h4>
+            <p data-aos="fade-up">Desenvolvedor Front, Back, 
             Mobile ou Full-Stack</p>
 
-            <h4>Linguagens de Programação mais usadas</h4>
-            <p>JavaScript, Python, Php, Java, C++, C#</p>
+            <h4 data-aos="fade-up">Linguagens de Programação mais usadas</h4>
+            <p data-aos="fade-up">JavaScript, Python, Php, Java, C++, C#</p>
 
-            <h4>Ferramentas mais usadas</h4>
-            <p>Visual Studio Code, WordPress, PhpStorm </p>
+            <h4 data-aos="fade-up">Ferramentas mais usadas</h4>
+            <p data-aos="fade-up">Visual Studio Code, WordPress, PhpStorm </p>
         </section>
 
         <section class="pt-comecar">
@@ -56,8 +67,12 @@
 <script src="js/menu.js"></script>
 <script src="js/slider.js"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<script>
-  AOS.init();
-</script>
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init({
+        duration: 800,
+        once: true
+      });
+    </script>
 </body>
 </html>
