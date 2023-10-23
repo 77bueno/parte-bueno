a2f87100be231e15762f319b6cc67577c14aa95e
victor <victorhugomotabueno@gmail.com>
2023-10-23 20:02:19 -0300

    atualização

--- index.php
+++ index.php
@@ -31,13 +31,83 @@
     </article>
 
 
+    <section class="py-10 bg-[#F2F7FF] sm:py-16 lg:py-24">
+        <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="text-center">
+                <h2 class="text-xl font-medium text-gray-900">Junte-se a milhares de pessoas que usam os nossos serviços</h2>
+    
+            <div class="grid grid-cols-1 gap-6 px-6 mt-8 sm:px-0 lg:mt-16 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-12">
+                <div class="overflow-hidden bg-white border border-gray-200 rounded-lg">
+                    <div class="px-4 py-6">
+                        <div class="flex items-start">
+                            <svg class="flex-shrink-0 w-12 h-12 text-[#FFBB00]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                            </svg>
+                            <div class="ml-4">
+                                <h4 class="text-4xl font-bold text-[#001b42]"><span class="contagem">559</span>+</h4>
+                                <p class="mt-1.5 text-lg font-medium leading-tight text-gray-500">Profissionais</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="overflow-hidden bg-white border border-gray-200 rounded-lg">
+                    <div class="px-4 py-6">
+                        <div class="flex items-start">
+                            <svg class="flex-shrink-0 w-12 h-12 text-[#FFBB00]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                            </svg>
+                            <div class="ml-4">
+                                <h4 class="text-4xl font-bold text-[#001b42]"><span class="contagem">1157</span>+</h4>
+                                <p class="mt-1.5 text-lg font-medium leading-tight text-gray-500">Usuários ativos</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="overflow-hidden bg-white border border-gray-200 rounded-lg">
+                    <div class="px-4 py-6">
+                        <div class="flex items-start">
+                            <svg class="flex-shrink-0 w-12 h-12 text-[#FFBB00]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                            </svg>
+                            <div class="ml-4">
+                                <h4 class="text-4xl font-bold text-[#001b42]"><span class="contagem">2450</span>+</h4>
+                                <p class="mt-1.5 text-lg font-medium leading-tight text-gray-500">Projetos entregues</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="overflow-hidden bg-white border border-gray-200 rounded-lg">
+                    <div class="px-4 py-6">
+                        <div class="flex items-start">
+                            <svg class="flex-shrink-0 w-12 h-12 text-[#FFBB00]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="1"
+                                    d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
+                                />
+                            </svg>
+                            <div class="ml-4">
+                                <h4 class="text-4xl font-bold text-[#001b42]"><span class="contagem">98</span>%</h4>
+                                <p class="mt-1.5 text-lg font-medium leading-tight text-gray-500">Sucesso do cliente</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
     <article class="pq-codelance">
         <img data-aos="zoom-in" src="imagens/ilustracao2.svg" alt="ilustração do site">
 
         <h2 data-aos="fade-up" class="pc-titulo">Por Que a codelance?</h2>
         <p data-aos="fade-up">Estamos aqui para simplificar o processo de encontrar os talentos certos para seus projetos, <b>economizando seu tempo e recursos</b>. Deixe-nos cuidar dos detalhes para que você possa se concentrar no que faz de melhor: alcançar seus objetivos.</p>
     </article>
-        
     
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#001b42" fill-opacity="1" d="M0,32L80,69.3C160,107,320,181,480,176C640,171,800,85,960,58.7C1120,32,1280,64,1360,80L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
         <section class="areas-de-atuacao">
@@ -45,6 +115,7 @@
             <h2 data-aos="fade-up">Áreas de Atuação</h2>
             <p data-aos="fade-up">Nos destacamos como uma poucas plataformas de freelancers para <b>desenvolvedores</b>. Oferecemos um ambiente inovador onde talentos técnicos podem encontrar projetos sob medida, enquanto empresas acessam expertise de alta qualidade. Nossa abordagem revolucionária impulsiona o sucesso de freelancers e clientes, moldando o futuro do trabalho independente. </p>
 
+            
             <h4 data-aos="fade-up">O que você precisa está aqui</h4>
             <ul>
                 <li data-aos="fade-up">Desenvolva/Atualize seu website.</li>
@@ -69,21 +140,20 @@
       once: true
     });
 
-    <script>
-        $(document).ready(function() {
-                $('.contagem').each(function () {
-                    var $this = $(this);
-                    $({ Counter: 0 }).animate({
-                        Counter: $this.text()
-                    }, {
-                        duration: 3000,
-                        easing: 'swing',
-                        step: function (now) {
-                            $this.text(Math.ceil(now));
-                        }
-                    });
+    $(document).ready(function() {
+            $('.contagem').each(function () {
+                var $this = $(this);
+                $({ Counter: 0 }).animate({
+                    Counter: $this.text()
+                }, {
+                    duration: 3000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $this.text(Math.ceil(now));
+                    }
                 });
             });
+        });
     </script>
 </script>
 </body>
